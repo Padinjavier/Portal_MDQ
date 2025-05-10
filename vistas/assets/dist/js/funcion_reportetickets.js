2807e8fde04a3cee37f9f8ffb37fd311aa654d0d
xavi_661 <javierpadin661@gmail.com>
2025-05-10 11:14:17 -0500

    seccion repostetickets lista

---
+++ vistas/assets/dist/js/funcion_reportetickets.js
@@ -0,0 +1,14 @@
+// C:\wamp64\www\Portal_MDQ\vistas\assets\dist\js\funcion_tickets.js
+$(document).ready(function () {
+    // CargarDatosTickets();
+    // SelectProblemasySubproblemas();
+    // SelectNombre();
+    // verificarAgregarSelectSoporte();
+});
+// FIN FUNCIONAMIENTO DE TABLA Tickets
+
+
+
+
+
+
